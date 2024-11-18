b77639a996a4d2915d26fe9c8be900fed4e02a3c
ferran <1751659@uab.cat>
2024-11-18 09:33:53 +0100

    Commit 4: Compilació 3 arxiu pràctica 3

--- practica3.c
+++ practica3.c
@@ -1,18 +1,30 @@
+//1.
 //int main(){ 
 //    int i,j; 
-//   i = 1; 
+//    i = 1; 
 //    j = 2; 
 //    i++; 
 //    j += i; 
 //    return 0; 
 //} 
 
+//2.
+//int main(){ 
+//    short i,j; 
+//    i = 1; 
+//    j = 2; 
+//    i++; 
+//    j += i;
+//    return 0; 
+//} 
 
-int main(){ 
-    short i,j; 
-    i = 1; 
-    j = 2; 
-    i++; 
-    j += i;
-    return 0; 
-} 
+//3.
+int main(){
+    int a,b,c,d;
+    b = 2;
+    c = 3;
+    d = 4;
+    a = b+c*d;
+    a = (b+c)*d;
+    d = a;
+}
