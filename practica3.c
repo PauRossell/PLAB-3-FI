e251728a2acad2bbcae02cf5d29d99671e0f667d
ferran <1751659@uab.cat>
2024-11-18 13:50:32 +0100

    Commit 8: Compilació 7 arxiu pràctica 3

--- practica3.c
+++ practica3.c
@@ -45,10 +45,20 @@
 //}
 
 //5.
-int main(){
-    char a[3];
-    a[0] = 1;
-    a[1] = 2;
-    a[2] = 3;
-    return 0;
+//int main(){
+//    char a[3];
+//    a[0] = 1;
+//    a[1] = 2;
+//    a[2] = 3;
+//    return 0;
+//}
+
+//6.
+int main() {
+    int a[4] = {1, 2, 3, 4};
+    int b[4] = {5, 6, 7, 8};
+    int c[4]; 
+    for (int i = 0; i < 4; i++) {
+        c[i] = a[i] * b[i];
+    }
 }
